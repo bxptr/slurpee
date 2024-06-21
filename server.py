82a62ee7392be30048c4d83c618db5467f20aa47
Aarush Gupta <git@aarushgupta.com>
2024-06-21 11:59:06 -0700

    fix default link order (recent first)

--- server.py
+++ server.py
@@ -90,6 +90,7 @@ def route_index() -> object:
         i = i.copy()
         i.pop("vector")
         clean.append(i)
+    clean = reversed(clean)
     response = flask.make_response(flask.render_template("index.html", index = clean))
     # don't cache
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
